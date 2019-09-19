e9437577821ae7bbb5199c59c2dab08e84e8764b
HuydDo <huydinhdo@yahoo.com>
2019-09-18 18:00:17 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,14 @@ class App < Sinatra::Base
 
     get '/' do
       erb :'../views/super_hero'
+     
     end
 
     post '/teams' do
       @team_name = params["team"]["name"]
       @team_motto = params["team"]["motto"]
       @team_members = params["team"]["members"]
-
+      # binding.pry
       erb :'../views/team'
     end
 
