b075027bc19c8d581f707d1d90b564a68fff8238
scrabill <shannon.crabill@gmail.com>
2019-09-20 14:10:06 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,19 @@ class App < Sinatra::Base
       erb :super_hero
     end
 
-    post "teams" do
+    post "/teams" do
+
+      # @team_name = team[name]
+
+      # "Hello"
+
       erb :team
+
+      # params["team"]["name"]
     end
+
+    # {"name"=>"The Wesley Crushers", "motto"=>"Shut up!", "members"=>[{"name"=>"Blazing Bev", "power"=>"Smoke", "bio"=>"Dr"}, {"name"=>"Riker", "power"=>"Leg lift", "bio"=>"Player"}, {"name"=>"Picard", "power"=>"Scalding Tea", "bio"=>"British"}]}
+
 
 
 end
